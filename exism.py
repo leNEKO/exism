231166e65fd733fb4087942db35ef795a4a7032c
neko <le.neko@gmail.com>
2018-06-12 20:41:45 +0200

    First commit

---
+++ exism.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import re
+import subprocess as sp
+import sys
+import os
+import shutil
+
+LANG = [
+    "python",
+    "php",
+    "javascript"
+]
+DIR = os.path.dirname(os.path.realpath(__file__))
+
+
+def create_task(lang: str, path: str):
+    exism = os.path.basename(path)
+    tf_path = f"{DIR}/task/{lang}.jsonc"
+    od_path = f"{path}/.vscode/"
+    of_path = f"{od_path}tasks.json"
+
+    if os.path.isfile(tf_path) and not os.path.isfile(of_path):
+        os.makedirs(od_path, exist_ok=True)
+        with open(tf_path, 'r') as f:
+            output = f.read().replace("###file###", exism)
+        with open(of_path, 'w') as f:
+            f.write(output)
+        print(f"Copied {of_path}")
+
+
+def main():
+
+    command = sys.argv
+    command[0] = "exercism"
+    output: str = sp.check_output(" ".join(command), shell=True,
+                                  universal_newlines=True).strip()
+    print(output)
+
+    pattern = re.compile(r"(\w+)\s\((.*)\)\s+(.*)")
+    results = pattern.findall(output)
+    for lang, comment, path in results:
+        if lang in LANG:
+            create_task(lang, path)
+
+
+if __name__ == '__main__':
+    main()
